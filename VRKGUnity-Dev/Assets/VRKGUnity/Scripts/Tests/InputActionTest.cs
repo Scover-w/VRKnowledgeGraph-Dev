5bb94d86e882f5b9b9fb730bf50dae298a22e7f8
unknown <william.pls.pro@gmail.com>
2023-05-19 16:36:18 +0200

    Working capacitive touch

--- VRKGUnity-Dev/Assets/VRKGUnity/Scripts/Tests/InputActionTest.cs
+++ VRKGUnity-Dev/Assets/VRKGUnity/Scripts/Tests/InputActionTest.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Wave.Native;
+using Wave.Essence;
 
 public class InputActionTest : MonoBehaviour
 {
@@ -25,6 +27,45 @@ public class InputActionTest : MonoBehaviour
     private void Test2(Vector2 v2)
     {
         Debug.Log("Test Hope " + v2.ToString());
+    }
+
+    private void Update()
+    {
+        
+    }
+
+
+
+
+
+
+    private void UpdateTouchs()
+    {
+        // /!\ Don't work if controllers not in the fov during the launch of the app
+
+
+        //Debug.Log("-------");
+
+        if (WXRDevice.ButtonTouch(WVR_DeviceType.WVR_DeviceType_Controller_Right, WVR_InputId.WVR_InputId_Alias1_Thumbstick))
+            Debug.Log("Hope 1");
+
+        if (WXRDevice.ButtonTouch(WVR_DeviceType.WVR_DeviceType_Controller_Right, WVR_InputId.WVR_InputId_2))
+            Debug.Log("Hope 2");
+
+        if (WXRDevice.ButtonTouch(WVR_DeviceType.WVR_DeviceType_Controller_Right, WVR_InputId.WVR_InputId_Alias1_Grip))
+            Debug.Log("Hope 3");
+
+        if (WXRDevice.ButtonTouching(WVR_DeviceType.WVR_DeviceType_Controller_Right, WVR_InputId.WVR_InputId_Alias1_Grip))
+            Debug.Log("Hope 4");
+
+        if (WXRDevice.ButtonTouch(WVR_DeviceType.WVR_DeviceType_Controller_Right, WVR_InputId.WVR_InputId_Alias1_Trigger))
+            Debug.Log("Hope 5");
+
+        if (WXRDevice.ButtonTouching(WVR_DeviceType.WVR_DeviceType_Controller_Right, WVR_InputId.WVR_InputId_Alias1_Trigger))
+            Debug.Log("Hope 6");
+
+        //if (WXRDevice.(WVR_DeviceType.WVR_DeviceType_Controller_Right, WVR_InputId.WVR_InputId_Alias1_Grip))
+        //    Debug.Log("Hope 3");
     }
 
 
