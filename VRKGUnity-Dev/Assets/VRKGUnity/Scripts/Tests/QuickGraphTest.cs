1b16c27ea77ceaa0b0d25ed090652827678f1275
unknown <william.pls.pro@gmail.com>
2023-05-22 16:46:03 +0200

    Add QuikGraph package from nugetForUnity

---
+++ VRKGUnity-Dev/Assets/VRKGUnity/Scripts/Tests/QuickGraphTest.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using QuikGraph;
+
+public class QuickGraphTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        var t = new QuikGraph.Edge<int>(1, 2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
