95b49632bf2b941a57a02f74146bf4db743d6638
unknown <william.pls.pro@gmail.com>
2023-06-23 10:08:42 +0200

    Mini Graph follow watch

--- VRKGUnity-Dev/Assets/VRKGUnity/Scripts/Graph/MiniGraph.cs
+++ VRKGUnity-Dev/Assets/VRKGUnity/Scripts/Graph/MiniGraph.cs
@@ -3,15 +3,50 @@ using UnityEngine;
 public class MiniGraph : MonoBehaviour
 {
 
-    public Transform Tf { get { return _tf; } }
+    public Transform Tf { get { return _miniGraphTf; } }
 
     [SerializeField]
-    Transform _tf;
+    Transform _miniGraphTf;
 
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    Transform _watchTf;
+
+    [SerializeField]
+    Transform _gpsPointTf;
+
+    bool _inGPSMode = false;
+    bool _displayGPS = true;
+
+    float _deltaHeight = .5f;
+
+
+    private void Update()
     {
-       
+        if (!_inGPSMode)
+            return;
+
+
+        _miniGraphTf.position = _watchTf.position + _watchTf.up * _deltaHeight;
     }
 
+    [ContextMenu("SwitchMode")]
+    public void SwitchMode()
+    {
+        _inGPSMode = !_inGPSMode;
+    }
+
+    [ContextMenu("SwitchGPSVisibility")]
+    public void SwitchGPSVisibility()
+    {
+        if (!_inGPSMode)
+            return;
+
+        _displayGPS = !_displayGPS;
+        _miniGraphTf.gameObject.SetActive(_displayGPS);
+    }
+
+    public void UpdateGPSPoint(Vector3 normPosition)
+    {
+        // TODO : convert to miniGraph scale
+    }
 }
