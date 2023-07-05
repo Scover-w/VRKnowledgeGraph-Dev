0feea3061bdab72448ed09c6405e5bf6a2782ab4
unknown <william.pls.pro@gmail.com>
2023-07-05 12:28:31 +0200

    Change wrong boolean _displayLabelsLens

--- VRKGUnity-Dev/Assets/VRKGUnity/Scripts/UI/LabelNodgeManagerUI.cs
+++ VRKGUnity-Dev/Assets/VRKGUnity/Scripts/UI/LabelNodgeManagerUI.cs
@@ -534,7 +534,7 @@ public class LabelNodgeManagerUI : MonoBehaviour
 
     private void TryDisplaySubNodesLabels(List<Node> labelsToDisplay)
     {
-        if (_graphMode == GraphMode.Desk && !_displayLabelsDesk)
+        if (_graphMode == GraphMode.Desk && !_displayLabelsLens)
             return;
 
         if (_graphMode == GraphMode.Immersion) // Don't display label for watch
