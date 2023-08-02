0b5d36d23e2edafdf2d92cc828bc23246256b71c
unknown <william.pls.pro@gmail.com>
2023-08-02 14:49:19 +0200

    Auto Add Proximity and Active Collider Tools

--- VRKGUnity-Dev/Assets/VRKGUnity/Editor/AIDENToolsEditor.cs
+++ VRKGUnity-Dev/Assets/VRKGUnity/Editor/AIDENToolsEditor.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Diagnostics;
 using System.IO;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEditor.Callbacks;
 using UnityEditor.SceneManagement;
@@ -224,5 +225,96 @@ public class AIDENToolsEditor : MonoBehaviour
         }
 
         return null;
+    }
+
+
+    [MenuItem("AIDEN Tools/Add Basic Interaction UI GOs")]
+    private static void AddBasicInteractionGOs()
+    {
+        GameObject selectedObject = Selection.activeGameObject;
+
+        if (selectedObject == null)
+        {
+            Debug.LogWarning("No gameobject selected.");
+            return;
+        }
+
+        MonoBehaviour physicalUIScript = GetPhysicalUIScript();
+
+        if(physicalUIScript == null ) 
+        {
+            Debug.LogWarning("No Monobehavior that implement IPhysicalUI founded.");
+            return;
+        }
+
+        Transform selectedTf = selectedObject.transform;
+        RectTransform selectedRect = selectedObject.GetComponent<RectTransform>();
+
+        Transform proximityTf = new GameObject("Proximity").transform;
+        ResetTf(proximityTf);
+
+
+
+        proximityTf.tag = Tags.ProximityUI;
+        var sphereCol = proximityTf.AddComponent<SphereCollider>();
+        sphereCol.isTrigger = true;
+        var triggerA = proximityTf.AddComponent<TriggerPhysicalUI>();
+        SerializedObject serializedTriggerA = new SerializedObject(triggerA);
+        SerializedProperty isProximityProp = serializedTriggerA.FindProperty("_isProximity");
+        SerializedProperty physicalUIScriptPropA = serializedTriggerA.FindProperty("_physicalUIScript");
+        isProximityProp.boolValue = true;
+        physicalUIScriptPropA.objectReferenceValue = physicalUIScript;
+
+        serializedTriggerA.ApplyModifiedProperties();
+
+        float x = selectedRect.rect.width;
+        float y = selectedRect.rect.height;
+        float maxXY = (x > y) ? x : y;
+        sphereCol.radius = maxXY * .5f;
+
+
+        Transform activeTf = new GameObject("Active").transform;
+        ResetTf(activeTf);
+
+        activeTf.tag = Tags.ActiveUI;
+        var boxCol = activeTf.AddComponent<BoxCollider>();
+        boxCol.isTrigger = true;
+        var triggerB = activeTf.AddComponent<TriggerPhysicalUI>();
+        SerializedObject serializedTriggerB = new SerializedObject(triggerB);
+        SerializedProperty physicalUIScriptPropB = serializedTriggerB.FindProperty("_physicalUIScript");
+        physicalUIScriptPropB.objectReferenceValue = physicalUIScript;
+
+        serializedTriggerB.ApplyModifiedProperties();
+
+        boxCol.size = new Vector3(x, y, 80f);
+        boxCol.center = new Vector3(0, 0, 80f / 2);
+
+        Debug.Log("Basic Interaction UI GOs added !");
+
+
+        void ResetTf(Transform tf)
+        {
+            tf.parent = selectedTf;
+            tf.localPosition = Vector3.zero;
+            tf.localRotation = Quaternion.identity;
+            tf.localScale = new Vector3(1f, 1f, 1f);
+
+            tf.gameObject.layer = Layers.UI;
+        }
+
+        MonoBehaviour GetPhysicalUIScript()
+        {
+            var monos = selectedObject.GetComponents<MonoBehaviour>();
+
+            foreach (MonoBehaviour script in monos)
+            {
+                if (script is IPhysicalUI physicalUI)
+                {
+                    return script;
+                }
+            }
+
+            return null;
+        }
     }
 }
