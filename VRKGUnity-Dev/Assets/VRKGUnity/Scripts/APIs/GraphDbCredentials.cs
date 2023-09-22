9277db4ee868967312a76b687c4bbd652e1511b1
unknown <william.pls.pro@gmail.com>
2023-09-22 10:04:33 +0200

    GraphDbCredentials

---
+++ VRKGUnity-Dev/Assets/VRKGUnity/Scripts/APIs/GraphDbCredentials.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Used only in unity editor
+/// </summary>
+public class GraphDbCredentials
+{
+    public const string USERNAME = "";
+    public const string PASSWORD = "";
+}
